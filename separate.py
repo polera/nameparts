5c1ea6aa831d54c779f91bf1ea511729e22f1084
James Polera <j@polera.org>
2010-04-07 00:36:48 -0400

    Handles multiple suffixes now.

--- separate.py
+++ separate.py
@@ -10,16 +10,13 @@ class Name:
                  'VANDE', 'VANDEN', 'VANDER', 'VOOR', 'VER', 'AAN', 'MC', 'BEN','SAN',
                  'SAINZ']
   NON_NAME    = ['A.K.A.','AKA','A/K/A','F.K.A','FKA','F/K/A','N/K/A','FICTITIOUS']
-  CORP_ENTITY = ['N.A.','CORP.','CO.','INC','ASSOCIATES','SERVICE','LLC','LLP','PARTNERS','R/A','C/O']
+  CORP_ENTITY = ['NA','CORP','CO','INC','ASSOCIATES','SERVICE','LLC','LLP','PARTNERS','R/A','C/O', 'COUNTY','STATE']
   SUPPLEMENTAL_INFO = ['WIFE OF','HUSBAND OF','SON OF','DAUGHTER OF']
 
   def get_has_non_name_values(self):
-    try:
-      for non_name_text in self.NON_NAME:
-        if self.__full_name.upper().find(non_name_text) > -1:
-          return True
-    except AttributeError:
-      pass
+    for part in self.__split_name:
+      if part.upper() in self.NON_NAME:
+        return True
     return False
   has_non_name_values = property(get_has_non_name_values)
   
@@ -39,6 +36,13 @@ class Name:
     return False
   has_salutation = property(get_has_salutation)
 
+  def get_looks_corporate(self):
+    for part in self.__split_name:
+      if part.upper() in self.CORP_ENTITY:
+        return True
+    return False
+  looks_corporate = property(get_looks_corporate)
+  
   def get_has_suffix(self):
     if self.__split_name[-1].upper() in self.SUFFIXES:
       return True
@@ -58,9 +62,10 @@ class Name:
     return unicode("<separate.Name: '%s'>" % self.__full_name)
       
   def process_name(self):  
-    if self.__processed:
+    if self.__processed or self.looks_corporate or self.has_non_name_values:
       return
     self.__clean()
+
     name_parts = self.__split_name
     
     # Find salutation, save it, remove it
@@ -68,10 +73,17 @@ class Name:
       self.__salutation = name_parts[0]
       del name_parts[0]
 
-    # Find suffix, save it, remove it
+    # Find suffix (or suffixes), save it, remove it
     if self.has_suffix:
-      self.__suffix = name_parts[-1]
-      del name_parts[-1]
+      suffixes = []
+      suffix_present = True
+      while suffix_present:
+        if name_parts[-1].upper() in self.SUFFIXES:
+          suffixes.append(name_parts[-1])
+          del name_parts[-1]
+        else:
+          suffix_present = False
+      self.__suffix = ", ".join(suffixes)
     
     # Save first name, remove it
     self.__first_name = name_parts[0]
@@ -130,5 +142,5 @@ class Name:
             'middle_name': self.middle_name, 
             'suffix': self.suffix}
   as_dict = property(get_name_as_dict)
-    
+  
   
