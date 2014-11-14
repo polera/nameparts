1c7f233599411ba33129b5a6d2dc04eeae8d91b6
James Polera <james@uncryptic.com>
2014-11-13 19:07:13 -0500

    Updated to version 0.5.8

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if sys.argv[-1] == 'publish':
 
 settings.update(
   name='nameparts',
-  version='0.5.7',
+  version='0.5.8',
   description='Takes a full human name and splits it into individual parts',
   long_description=read('README.md'),
   author='James Polera',
